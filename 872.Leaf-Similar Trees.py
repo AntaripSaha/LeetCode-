80d676352a63ad6bb92f30347336e94463d9d45d
ANTARIP\Antarip Saha <antarip15@gmail.com>
2025-05-31 20:42:23 -0400

    similar leaf check

---
+++ 872.Leaf-Similar Trees.py
@@ -0,0 +1,65 @@
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def build_tree_level_order(values):
+    if not values or values[0] is None:
+        return None
+
+    root = TreeNode(values[0])
+    queue = deque([root])
+    i = 1
+
+    while queue and i < len(values):
+        current = queue.popleft()
+
+        # Left child
+        if values[i] is not None:
+            current.left = TreeNode(values[i])
+            queue.append(current.left)
+        i += 1
+
+        # Right child
+        if i < len(values) and values[i] is not None:
+            current.right = TreeNode(values[i])
+            queue.append(current.right)
+        i += 1
+    return root
+
+# to Collect leaves
+# def get_leaves(node, leaves):
+#     if not node:
+#         return
+#     if not node.left and not node.right:
+#         leaves.append(node.val)
+#     get_leaves(node.left, leaves)
+#     get_leaves(node.right, leaves)
+
+# Check Leaf Node
+
+def leafSimilar(root1, root2):
+    def get_leaves(node):
+        if not node:
+            return []
+        if not node.left and not node.right:
+            return [node.val]
+        left = get_leaves(node.left)
+        right = get_leaves(node.right)
+        combin = left + right
+        print(combin)
+        return combin
+
+    return get_leaves(root1) == get_leaves(root2)
+
+
+# Test input: LeetCode format
+root1 = [1,2,3]
+root2 = [1,3,2]
+tree1 = build_tree_level_order(root1)
+tree2 = build_tree_level_order(root2)
+
+print(leafSimilar(tree1, tree2))
