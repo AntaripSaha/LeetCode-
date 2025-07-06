c71a45c0dc32de08ad478dc9dd1e7ae8b6ed944d
ANTARIP\Antarip Saha <antarip15@gmail.com>
2025-07-05 20:27:42 -0400

    Two Integer Sum II

---
+++ Two Integer Sum II.py
@@ -0,0 +1,16 @@
+numbers = [1,2,3,4]
+target = 3
+
+l = 0
+r = len(numbers)-1
+
+while l<r:
+    sum = numbers[l] + numbers[r]
+
+    if sum > target:
+        r -=1
+    elif sum < target:
+        l += 1
+    else:
+        print([l+1, r+1])
+        exit()
